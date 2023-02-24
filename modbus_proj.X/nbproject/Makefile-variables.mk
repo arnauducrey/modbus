7136afef2bcb19cdf0ef4159f94463244e57a0c3
Arnaud <ducreyarn@gmail.com>
2023-02-24 15:08:12 +0100

    initialisation

---
+++ modbus_proj.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=modbus_proj.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/modbus_proj.X.production.hex
